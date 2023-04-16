3f1a4c90a2708afc87f0580d5afd89f33eca67f5
manasvipriya <priyamanasvi3@gmail.com>
2023-04-16 11:27:10 +0530

    updated

--- Variables.MyPlayground.playground/Contents.swift
+++ Variables.MyPlayground.playground/Contents.swift
@@ -1,9 +1,46 @@
 import Foundation
 
 let myName = "Manasvi"
-let yourName = "Jenna"
-
-myName = yourName
-
+let yourName = "Jenna"  // myName=yourName is wrong.
 //let creates immutability. Hence, let can't be assigned again.
-//my first program
+
+//Creating Arrays
+var names = [        //we can't use let here because we are assigning variabes again.
+myName,
+yourName
+]
+//names = ["Bla"]    ...doing this we can assign a totally new variable .
+
+//appending Arrays
+names.append("Bar")
+names.append("Baz")
+
+//Value types
+let foo = "Foo"
+var foo2 = foo
+foo2 = "Foo2"
+foo
+foo2
+
+//Structure(Value types)
+let moreNames=[
+"Foo",
+"Bar"
+]
+var copy = moreNames
+copy.append("Baz")
+moreNames
+copy
+
+//Class(reference types)
+let oldArray = NSMutableArray(    //NSMutableArray makes let mutable.
+    array:[
+    "Foo",
+    "Bar"
+    ]
+)
+oldArray.add("Baz")
+var newArray = oldArray
+newArray.add("Qux")
+oldArray
+newArray
