4dd907a955c5f82b55cefa26f3eba0052c66afae
manasvipriya <priyamanasvi3@gmail.com>
2023-04-16 14:03:04 +0530

    updated

---
+++ Operator.playground/Contents.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+//operators
+let myAge = 22
+let yourAge = 20
+ 
+if myAge > yourAge {    //">"
+   "I'm older than you"
+}else if myAge < yourAge{    //"<"
+    "I'm younger than you"
+}else{
+    "Oh hey! We're the same age."
+}
+
+let mymothersAge = myAge + 30   //"+"
+let doublemyAge = myAge * 2     //"*"
+ 
+
+//Three different types of OPERATORS:-
+//UNARY PREFIX
+//UNARY POSTFIX
+//BINARYINFIX
+
+
+
