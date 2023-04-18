e7404a5b122209e0e7eaf3942da9d73a34319cb7
manasvipriya <priyamanasvi3@gmail.com>
2023-04-18 11:35:10 +0530

    updated

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -21,4 +21,63 @@ newPlusTwo(value: 30)
 
 
 //functions with multiple arguments
+func customAdd(
+value1: Int,
+value2: Int) -> Int{
+    value1 + value2
+}
+let customadded = customAdd(
+    value1: 10,
+    value2: 20
+)
+
+
+func customSubtract(
+    lhs: Int,
+    rhs: Int
+) -> Int{
+    lhs - rhs
+}
+let customSubtracted = customSubtract(
+        lhs: 20,
+        rhs: 10
+)
+
+@discardableResult  //allows you to use the return value if you want while you can decide as well to just ignore it.
+func myCustomAdd(
+    _ lhs: Int,
+    _ rhs: Int
+) -> Int{
+    lhs + rhs
+}
+myCustomAdd(
+    20,
+    30
+)
+
+
+//functions contains their own functions
+func doSomethingComplicated(
+    with value: Int
+) -> Int{
+    func mainLogic(value: Int) -> Int{
+        value + 2
+    }
+    return mainLogic(value: value + 3)
+    
+}
+doSomethingComplicated(with: 30)
+
+
+func getFullName(
+    firstName: String = "Foo",
+    lastName: String = "Bar"
+) -> String{
+    "\(firstName) \(lastName)"
+}
+//calling the functions :
+//without arguments
+getFullName()
+
+    
 
