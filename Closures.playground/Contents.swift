d550a261a55449ebd5e917aa35b20874410438d4
manasvipriya <priyamanasvi3@gmail.com>
2023-04-18 12:33:55 +0530

    updated

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -14,3 +14,25 @@ let add: (Int, Int) -> Int
     lhs + rhs
 }
 add(20, 30)
+
+
+//demonstration of how you can pass a fucntion to another function and how you can create a function on the go
+func customAdd(
+    _ lhs: Int,
+    _ rhs: Int,
+    using function: (Int, Int) -> Int
+) -> Int{
+    function(lhs, rhs)
+}
+customAdd(
+    20,
+    30
+) { (lhs: Int, rhs: Int) -> Int in
+        lhs + rhs
+    }
+
+//passing special closures to functions
+let ages = [20, 30, 40, 50]
+ages.sorted {(lhs: Int, rhs: Int) -> Bool in
+    lhs > rhs
+}
