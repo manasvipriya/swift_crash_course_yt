668cf76347977c275e213808a3eb87619292ef6e
manasvipriya <priyamanasvi3@gmail.com>
2023-04-19 19:28:14 +0530

    updated

--- Structures.playground/Contents.swift
+++ Structures.playground/Contents.swift
@@ -23,5 +23,27 @@ let c64 = CommodoreComputer(
     name: "My Commodore",
     manufacturer: "Commodore"
 )
+//computed properties
 
+struct Person2 {
+    let firstName: String
+    let lastName: String
+    let fullName: String
+    init(
+        firstName: String,
+        lastName: String
+    ) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.fullName = "\(firstName) \(lastName)"
+    }
+}
+//computed properties are removing all these above and just using var at place of let.
 
+struct Person2 {
+    let firstName: String
+    let lastName: String
+    var fullName: String{
+        "\(firstName) \(lastName)"
+    }
+}
