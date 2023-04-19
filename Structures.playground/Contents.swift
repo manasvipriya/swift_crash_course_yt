1185e52f719bba8cf7f3ec0b5603c08f9529cc42
manasvipriya <priyamanasvi3@gmail.com>
2023-04-19 13:27:21 +0530

    updated

---
+++ Structures.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+// way of grouping data together and give it a name
+struct person{
+    let name: String
+    let age: Int
+}
+ let foo = Person(
+    name: "Foo",
+    age: 20
+ )
+
+foo.name
+foo.age
