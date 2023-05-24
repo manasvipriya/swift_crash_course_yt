d42b4533788decb0f753926b099d86c88bf46572
manasvipriya <priyamanasvi3@gmail.com>
2023-05-24 10:44:37 +0530

    updated

--- enumerations.playground/Contents.swift
+++ enumerations.playground/Contents.swift
@@ -31,11 +31,30 @@ default:
     
 }
 
-enum FamilyMember{
-    case father
-    case mother
-    case brother
-    case sister
+enum FamilyMember: String{
+    case father = "Dad"
+    case mother = "Mom"
+    case brother = "Bro"
+    case sister = "Sis"
+}
+FamilyMember.father.rawValue
+FamilyMember.mother.rawValue
+FamilyMember.brother.rawValue
+FamilyMember.sister.rawValue
+
+enum FavouriteEmoji: String, CaseIterable{
+    case blush = "😊"
+    case rocket = "🚀"
+    case fire = "🔥"
+}
+FavouriteEmoji.allCases
+FavouriteEmoji.allCases.map(\.rawValue)
+
+if let blush = FavouriteEmoji(rawValue: "😊"){
+    "Found the blush emoji"
+    blush
+}else{
+    "This emoji doesn't exist"
 }
 
 
